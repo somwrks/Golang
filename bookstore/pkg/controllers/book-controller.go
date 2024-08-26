9530061e8c3e89c5c4e7fb634b938536a7e9c662
Som S. <somwrks@gmail.com>
2024-08-26 18:23:31 +0000

    e

--- bookstore/pkg/controllers/book-controller.go
+++ bookstore/pkg/controllers/book-controller.go
@@ -64,7 +64,7 @@ func UpdateBook ( w http.ResponseWriter , r *http.Request){
 	var updateBook = &models.Book{}
 	utils.ParseBody(r, updateBook)
 	vars := mux.Vars(r)
-	bookId:= vars ["bookId"]
+	bookId:= vars ["bookId"] 
 	ID, err := strconv.ParseInt(bookId, 0 ,0 )
 	if err !=nil{
 		fmt.Println("error while parsing")
