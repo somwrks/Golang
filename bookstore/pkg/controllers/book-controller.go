6644e2e7a4c15c1fc988539dd4bac99963580836
Som S. <somwrks@gmail.com>
2024-08-26 18:06:13 +0000

    e

--- bookstore/pkg/controllers/book-controller.go
+++ bookstore/pkg/controllers/book-controller.go
@@ -59,3 +59,26 @@ func DeleteBook( w http.ResponseWriter,  r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func UpdateBook ( w http.ResponseWriter , r *http.Request){
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId:= vars ["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0 ,0 )
+	if err !=nil{
+		fmt.Println("error while parsing")
+	}
+	bookDetails, db := models.GetBookById(ID)
+	if updateBook.Name!=""{
+			bookDetails.Name = updateBook.Name
+	}
+	if updateBook.Author!=""{
+			bookDetails.Author = updateBook.Author
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
