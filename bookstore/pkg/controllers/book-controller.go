6b4aaa1a710377f727f7d64fd0d25e8eab1aa6eb
Som S. <somwrks@gmail.com>
2024-08-26 18:00:27 +0000

    e

--- bookstore/pkg/controllers/book-controller.go
+++ bookstore/pkg/controllers/book-controller.go
@@ -8,17 +8,18 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/somwrks/Golang/bookstore/pkg/models"
+	"github.com/somwrks/Golang/bookstore/pkg/utils"
 	"github.com/somwrks/golang/bookstore/pkg/models"
 	"github.com/somwrks/golang/bookstore/pkg/utils"
 )
 
 var NewBook models.Book
 
-func GetBook (w http.Request, r *http.Response){
+func GetBook (w http.ResponseWriter, r *http.Request){
 	newBooks:=models.GetAllBooks()
 	res , _ :=json.Marshal(newBooks)
 	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriterHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
 
@@ -30,4 +31,31 @@ func GetBookById(w http.ResponseWriter, r *http.Request){
 		fmt.Println("error while parsing")
 	}
 	bookDetails, _ :=models.GetBookById(ID)
+	res , _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook( w http.ResponseWriter, r *http.Request){
+	CreateBook := &models.Book{}
+	utils.ParseBody(r , CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ :=json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook( w http.ResponseWriter,  r *http.Request){
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0,0)
+	if err !=nil {
+		fmt.Println("error while parsing")
+	}
+	book:=models.DeleteBook(ID)
+	res, _ :=json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
